f606eed780ca7abf9a3057541d105e802c790829
Isaac Zhang <isaac-z@outlook.com>
2021-11-27 14:55:53 +0800

    update

---
+++ ClionProject/cmake-build-debug/CMakeFiles/ClionProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionProject.dir/main.c.obj"
+  "ClionProject.exe"
+  "ClionProject.exe.manifest"
+  "ClionProject.pdb"
+  "libClionProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ClionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
